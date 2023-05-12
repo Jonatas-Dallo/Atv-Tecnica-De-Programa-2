bf43becb3be9e45649c0dfe467f3f5bef3293f66
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2023-05-12 11:12:46 -0300

    Atividade 1 clonagem do documento e telefone do titular pro dependente

--- modelos/telefone.ts
+++ modelos/telefone.ts
@@ -1,4 +1,13 @@
-export default class Telefone {
+import Prototipo from "../interfaces/prototipo"
+
+export default class Telefone implements Prototipo{
     public ddd: string
     public numero: string
+
+    public clonar(): Prototipo {
+        let telefones = new Telefone()
+        telefones.ddd = this.ddd
+        telefones.numero = this.numero
+        return telefones
+    }
 }
--- teste/index.ts
+++ teste/index.ts
@@ -1,10 +1,30 @@
 import Cliente from "../modelos/cliente";
+import Documento from "../modelos/documento";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone"
+import { TipoDocumento } from "../enumeracoes/tipoDocumento"
+
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
 cliente.dataCadastro = new Date(1840, 6, 23)
 cliente.dataNascimento = new Date(1825, 11, 2)
+
+let documento = new Documento()
+documento.numero = "123.456.789-10"
+documento.tipo = TipoDocumento.CPF;
+documento.dataExpedicao = new Date(2023, 3, 1);
+cliente.documentos.push(documento)
+
+let telefone1 = new Telefone()
+telefone1.ddd = "012"
+telefone1.numero = "4002-8922"
+cliente.telefones.push(telefone1)
+let telefone2 = new Telefone()
+telefone2.ddd = "012"
+telefone2.numero = "98894-1043"
+cliente.telefones.push(telefone2)
+
 let endereco = new Endereco()
 endereco.rua = `R. do Catete`
 endereco.bairro = `Copacabana`
@@ -20,8 +40,17 @@ dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones = [];
+cliente.telefones.forEach((telefone: Telefone) => {
+  dependente.telefones.push(telefone.clonar() as Telefone);
+});
+dependente.documentos = [];
+cliente.documentos.forEach((documentos: Documento) => {
+  dependente.documentos.push(documento.clonar() as Documento);
+});
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
 console.log(cliente);
+console.log("---------------------")
 console.log(dependente);
